e1b01376f0b2603afe85cc7b0de68e5b8871e66c
anandsundramurthy <anand.sundramurthy@zattoo.com>
2023-09-05 15:04:33 +0200

    Update

--- playwright.config.js
+++ playwright.config.js
@@ -12,6 +12,7 @@ const { defineConfig, devices } = require('@playwright/test');
  */
 module.exports = defineConfig({
   snapshotPathTemplate: 'tests/snapshots/{arg}{ext}',
+  updateSnapshots: 'all',
   testDir: './tests',
   /* Run tests in files in parallel */
   fullyParallel: true,
